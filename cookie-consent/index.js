55142cc62e8dbacad124f0f31127d2875d6523ce
Sachini <sachichathudissanayaka@gmail.com>
2024-07-17 19:55:58 +0530

    Add changes

--- cookie-consent/index.js
+++ cookie-consent/index.js
@@ -1,8 +1,5 @@
-/*
-Challenge:
-1. Log out the phrase "Modal Opened!" 
-   after a 1.5 second delay.
-*/
+const modal = document.getElementById('modal')
+
 setTimeout(function(){
-    console.log("Model opened!")
+   modal.style.display = 'block'
  }, 1500)
