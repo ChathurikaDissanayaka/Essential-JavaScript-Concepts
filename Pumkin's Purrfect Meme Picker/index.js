842df72a60180992e94bb3f31e6efeee9ac90a1e
Sachini <sachichathudissanayaka@gmail.com>
2024-08-12 19:05:43 +0530

    Add changes

--- Pumkin's Purrfect Meme Picker/index.js
+++ Pumkin's Purrfect Meme Picker/index.js
@@ -2,11 +2,17 @@ import { catsData } from './data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
 
+emotionRadios.addEventListener('change', function(e){
+    console.log(e.target.id)
+})
+
 function getEmotionsArray(cats){
     const emotionsArray = []
     for (let cat of cats){
         for (let emotion of cat.emotionTags){
-            emotionsArray.push(emotion)
+            if(!emotionsArray.includes(emotion)){
+                emotionsArray.push(emotion)
+            }
         }
     }
     return emotionsArray
