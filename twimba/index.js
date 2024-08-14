0dfaf0761df856aaba94ec40cedf0e319e01e1d5
Sachini <sachichathudissanayaka@gmail.com>
2024-08-14 21:32:15 +0530

    Add changes

--- twimba/index.js
+++ twimba/index.js
@@ -1,8 +1,71 @@
 import { tweetsData } from './data.js'
-
 const tweetInput = document.getElementById('tweet-input')
 const tweetBtn = document.getElementById('tweet-btn')
 
 tweetBtn.addEventListener('click', function(){
     console.log(tweetInput.value)
 })
+
+function getFeedHtml(){
+/*
+Challenge:
+1. Use a "for of" to iterate over the data and 
+   create HTML string for each tweet using the 
+   boilerplate below. Replace UPPERCASE text
+   with data from the tweets. 
+2. Store this HTML in a let called "feedHtml".
+3. Log out feedHtml.
+4. Call getFeedHtml to check it's working.
+*/  
+    let feedHtml =""
+    for(let tweetData of tweetsData){
+        feedHtml += `<div class="tweet">
+        <div class="tweet-inner">
+            <img src="${tweetData.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${tweetData.handle}</p>
+                <p class="tweet-text">${tweetData.tweetText}</p>
+                <div class="tweet-details">
+                    <span class="tweet-detail">
+                        ${tweetData.replies.length}
+                    </span>
+                    <span class="tweet-detail">
+                        ${tweetData.likes}
+                    </span>
+                    <span class="tweet-detail">
+                        ${tweetData.retweets}
+                    </span>
+                </div>   
+            </div>            
+        </div>
+    </div>`
+    }
+    return feedHtml
+}
+
+getFeedHtml()
+
+/*
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="URL OF PROFILE PIC" class="profile-pic">
+        <div>
+            <p class="handle">TWEET HANDLE</p>
+            <p class="tweet-text">TWEET TEXT</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    NUMBER OF REPLIES
+                </span>
+                <span class="tweet-detail">
+                    NUMBER OF LIKES
+                </span>
+                <span class="tweet-detail">
+                    NUMBER OF RETWEETS
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>
+
+*/
+
