a3842b2d6b2cb7174a3bbd1a7a839ac35a75e1e0
Sachini <sachichathudissanayaka@gmail.com>
2024-08-14 21:37:17 +0530

    Add changes

--- twimba/index.js
+++ twimba/index.js
@@ -7,65 +7,30 @@ tweetBtn.addEventListener('click', function(){
 })
 
 function getFeedHtml(){
-/*
-Challenge:
-1. Use a "for of" to iterate over the data and 
-   create HTML string for each tweet using the 
-   boilerplate below. Replace UPPERCASE text
-   with data from the tweets. 
-2. Store this HTML in a let called "feedHtml".
-3. Log out feedHtml.
-4. Call getFeedHtml to check it's working.
-*/  
     let feedHtml =""
-    for(let tweetData of tweetsData){
+    tweetsData.forEach(function(tweet){
         feedHtml += `<div class="tweet">
-        <div class="tweet-inner">
-            <img src="${tweetData.profilePic}" class="profile-pic">
-            <div>
-                <p class="handle">${tweetData.handle}</p>
-                <p class="tweet-text">${tweetData.tweetText}</p>
-                <div class="tweet-details">
-                    <span class="tweet-detail">
-                        ${tweetData.replies.length}
-                    </span>
-                    <span class="tweet-detail">
-                        ${tweetData.likes}
-                    </span>
-                    <span class="tweet-detail">
-                        ${tweetData.retweets}
-                    </span>
-                </div>   
-            </div>            
-        </div>
-    </div>`
-    }
+            <div class="tweet-inner">
+                <img src="${tweet.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">
+                            ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-detail">
+                            ${tweet.likes}
+                        </span>
+                        <span class="tweet-detail">
+                            ${tweet.retweets}
+                        </span>
+                    </div>   
+                </div>            
+            </div>
+        </div>`
+    })
     return feedHtml
 }
 
 getFeedHtml()
-
-/*
-<div class="tweet">
-    <div class="tweet-inner">
-        <img src="URL OF PROFILE PIC" class="profile-pic">
-        <div>
-            <p class="handle">TWEET HANDLE</p>
-            <p class="tweet-text">TWEET TEXT</p>
-            <div class="tweet-details">
-                <span class="tweet-detail">
-                    NUMBER OF REPLIES
-                </span>
-                <span class="tweet-detail">
-                    NUMBER OF LIKES
-                </span>
-                <span class="tweet-detail">
-                    NUMBER OF RETWEETS
-                </span>
-            </div>   
-        </div>            
-    </div>
-</div>
-
-*/
-
