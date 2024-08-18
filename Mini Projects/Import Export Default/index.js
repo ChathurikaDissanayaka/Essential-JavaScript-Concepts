7b241d0cc36722dab0f990a5da1839a82d0bdb8f
Sachini <sachichathudissanayaka@gmail.com>
2024-08-18 21:41:57 +0530

    Add changes

--- Mini Projects/Import Export Default/index.js
+++ Mini Projects/Import Export Default/index.js
@@ -1,4 +1,4 @@
-import { interplanetaryDestinationsArr, shortSpaceTripsArr } from '/data.js'
-import getMatchingTripsArr from '/getMatchingTripsArr.js'
+import { interplanetaryDestinationsArr, shortSpaceTripsArr } from './data.js'
+import getMatchingTripsArr from './getMatchingTripsArr.js'
 
 console.log(getMatchingTripsArr(interplanetaryDestinationsArr, 'exotic'))
