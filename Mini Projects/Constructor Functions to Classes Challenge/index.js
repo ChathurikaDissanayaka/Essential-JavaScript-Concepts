595398b9237cdea6298ade20ab5cec97f96c89f2
Sachini <sachichathudissanayaka@gmail.com>
2024-09-11 22:34:59 +0530

    Add changes

---
+++ Mini Projects/Constructor Functions to Classes Challenge/index.js
@@ -0,0 +1,27 @@
+/*
+Challenge:
+Rebuild this constructor function as a class.
+*/
+class Character{
+    constructor(name) {
+        this.name = name
+        this.collectedItemsArr = []
+    }
+    
+    addItem(item) {
+        this.collectedItemsArr.push(item)
+        console.log(`${this.name} now has: ${this.collectedItemsArr.join(', ')}`)
+    }
+}
+
+// function Character(name) {
+//     this.name = name
+//     this.collectedItemsArr = []
+//     this.addItem = function (item) {
+//         this.collectedItemsArr.push(item)
+//         console.log(`${this.name} now has: ${this.collectedItemsArr.join(', ')}`)
+//     }
+// }
+
+const wizard = new Character('Merlin')
+wizard.addItem('a wand')
