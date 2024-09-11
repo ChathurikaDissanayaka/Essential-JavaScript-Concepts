cd8c3eadf306411e2bc92881f5d18e87ab8e136f
Sachini <sachichathudissanayaka@gmail.com>
2024-09-12 01:26:45 +0530

    Add changes

--- Solo Project/index.js
+++ Solo Project/index.js
@@ -1,6 +1,7 @@
 import { menuArray } from './data.js'
 
 const menu = document.getElementById('menu')
+const containerEl = document.getElementById('container')
 
 // Render menu items
 function renderMenuItems(){
@@ -13,10 +14,46 @@ function renderMenuItems(){
                 <h3>$${item.price}</h3>
             </div>
             <div class="btn">
-                <button class="add-btn">+</button>
+                <button id="add-btn" class="add-btn">+</button>
             </div>
         </section>`
     });
 }
 
 renderMenuItems()
+
+// const addBtn = document.getElementById('add-btn')
+
+// addBtn.addEventListener('click', function(){
+//     containerEl.innerHTML += `<div class="bill"></div>`
+// })
+
+// render this when add item buttons clicked
+// `<div class="bill">
+//                 <h2 class="title">Your order</h2>
+//                 <div class="order-items">
+//                     <div class="order-item">
+//                         <h2>Pizza</h2>
+//                         <button class="remove">remove</button>
+//                         <p>$14</p>
+//                     </div>
+//                     <div class="order-item">
+//                         <h2>Beer</h2>
+//                         <button class="remove">remove</button>
+//                         <p>$14</p>
+//                     </div>
+//                 </div>
+            
+//                 <div class="total-price">
+//                     <h2>Total price:</h2>
+//                     <p>$26</p>
+//                 </div>
+
+//                 <div class="complte-order"></div>
+//                 <button class="complete-order-btn">Complete order</button>
+//             </div>`
+
+// Render this when order complete clicked
+// `<div class="msg">
+//      <p>Thanks, James! Your order is on its way!</p>
+//  </div>`
